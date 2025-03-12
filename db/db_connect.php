bbca66f5b0c1149d359b74615891888a9346733d
B1jam1-74 <bavocatmaulaz@gmail.com>
2025-03-12 11:29:44 +0100

    ajout connexion BDD

--- db/db_connect.php
+++ db/db_connect.php
@@ -1,9 +1,27 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+define ('SERVEUR_BD','localhost');
+define ('LOGIN_BD','root');
+define ('PASS_BD','');
+define ('NOM_BD','info411');
 
-$conn = mysqli_connect("localhost", "grp2","aeb7naiJ", "db_grp2");
-mysqli_set_charset($conn, "utf8");
+
+$CONNEXION = mysqli_connect (SERVEUR_BD, LOGIN_BD, PASS_BD);
+	//Connexion au serveur de bases de données
+	if (mysqli_connect_errno()) {
+		echo 'Désolé, connexion au serveur ' . SERVEUR_BD . ' impossible, '. mysqli_connect_error(), "\n";
+    	exit();
+	}
+	// Sélection de la base de données
+	mysqli_select_db($CONNEXION, NOM_BD);
+	if (mysqli_connect_errno()) {
+		echo 'Désolé, accès à la base ' . NOM_BD . ' impossible, '. mysqli_connect_error(), "\n";
+    	exit();
+	}
+	// Spécification de l'encodage UTF-8 pour dialoguer avec la BD
+	if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
+    	echo 'Erreur au chargement de l\'encodage UTF-8 : ', mysqli_connect_error(), "\n";
+	}
+
 
 /**
  * Fonction auxiliaire pour transformer un rs en tableau
