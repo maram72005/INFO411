05e1740f66aa7350e50fd3e300dccf9aab935068
B1jam1-74 <bavocatmaulaz@gmail.com>
2025-03-12 18:38:18 +0100

    essai de l'affichage des produits à l'aide de la BDD

--- site.php
+++ site.php
@@ -18,7 +18,32 @@ $user = $_SESSION["username"];
 
 print_r($user);
 
+function list_products($conn) {
+    $res = mysqli_query($conn, "select * from article") ;
+	$tab=[] ; 
+	while($row=mysqli_fetch_assoc($res)){
+		$tab[]=$row ;	
+	}
+	return $tab;
+}
+
+// print_r(list_products($conn));
+
+//1- Récupère la liste des articles
+$products=list_products($conn) ;
+
+//2- Transforme le tableau php en tableau Js
+$products_str=json_encode($products);
+
 ?>
+
+<script> 
+<?php 
+echo "const products = ${products_str}";
+?> 
+
+console.log(products);
+</script>
 
 <h1> Bienvenue sur Model-Shop ! </h1>
 
